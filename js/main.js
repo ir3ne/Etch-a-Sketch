c8ef1550f87c92a1e9daf8f9fc833b70c15fff60
Irene <irene.tomaini@docplanner.com>
2021-01-23 10:48:38 +0100

    set grid size

--- js/main.js
+++ js/main.js
@@ -4,7 +4,13 @@ let gridBgColor = '#fafafa';
 const gridContainer = document.querySelector('.grid-container');
 gridContainer.style.backgroundColor = gridBgColor;
 
-const createGrid = () => {
+const gridSizeControl = document.getElementById('gridsize');
+const gridSizeControlLabel = document.getElementById('gridsize-label');
+const gridSizeControlValue = gridSizeControl.getAttribute('value');
+gridSizeControlLabel.textContent = gridSizeControlValue;
+
+const createGrid = (gridSize = 20) => {
+  gridContainer.innerHTML = '';
   gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
@@ -16,3 +22,9 @@ const createGrid = () => {
 }
 
 createGrid();
+
+gridSizeControl.addEventListener('change', (e) => {
+  let gridSizeSelected = e.target.value;
+  gridSizeControlLabel.textContent = gridSizeSelected;
+  createGrid(gridSizeSelected);
+});
