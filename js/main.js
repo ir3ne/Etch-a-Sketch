b7f25991b97e0b0e48298483cb187645b6e480a7
Irene <irene.tomaini@docplanner.com>
2021-01-23 10:24:58 +0100

    basic grid create

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log('it works');
+let gridSize = 20;
+let gridBgColor = '#fafafa';
+
+const gridContainer = document.querySelector('.grid-container');
+gridContainer.style.backgroundColor = gridBgColor;
+
+const createGrid = () => {
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+  for (let i = 0; i < gridSize ** 2; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
+    gridContainer.appendChild(gridItem);
+  }
+}
+
+createGrid();
